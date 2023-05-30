0b0764dcdc1493c56d5c1f2f1df235a0ae1af066
Thibesan Raveenderan <traveen2@uwo.ca>
2023-05-29 21:49:43 -0400

    Middleware, change to page router

--- netflix-clone/app/api/[...nextauth].ts
+++ netflix-clone/app/api/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
-import prismadb from '../lib/prismadb'
+import prismadb from '../../lib/prismadb'
 import { compare } from 'bcrypt'
 
 export default NextAuth({
