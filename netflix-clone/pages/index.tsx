7e7c18c538284aa23dbaed8257d501691c80b6b9
Thibesan Raveenderan <traveen2@uwo.ca>
2023-05-31 19:00:54 -0400

    Functional Template for Profiles Screen

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -3,33 +3,32 @@ import { NextPageContext } from 'next'
 import { getSession, signOut } from 'next-auth/react'
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context)
-  
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      }
-    }
-  }
-  
-  return {
-    props: {}
-  }
+	const session = await getSession(context)
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/auth',
+				permanent: false,
+			},
+		}
+	}
+
+	return {
+		props: {},
+	}
 }
 
-
-
 export default function Home() {
-  const { data: user } = useCurrentUser()
-  
-  
-  return (
-    <div>
-    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-    <p className='text-white'>Logged in as: {user?.email}</p>
-    <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout</button>
-    </div>
-  )
+	const { data: user } = useCurrentUser()
+
+	return (
+		<div>
+			<h1 className="text-2xl text-green-500">Netflix Clone</h1>
+			<p className="text-white">Logged in as: {user?.email}</p>
+			<button className="h-10 w-full bg-white" onClick={() => signOut()}>
+				Logout
+			</button>
+		</div>
+	)
 }
