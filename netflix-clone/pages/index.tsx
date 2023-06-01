7bfa150928218dd314f6ba8ef4802b87ce70c619
Thibesan Raveenderan <traveen2@uwo.ca>
2023-05-31 22:48:43 -0400

    Main Navbar Component with respective headers and updated sign out function

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from '@/hooks/useCurrentUser'
 import { NextPageContext } from 'next'
 import { getSession, signOut } from 'next-auth/react'
+import Navbar from '../components/Navbar'
 
 export async function getServerSideProps(context: NextPageContext) {
 	const session = await getSession(context)
@@ -22,13 +23,7 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
 	const { data: user } = useCurrentUser()
 
-	return (
-		<div>
-			<h1 className="text-2xl text-green-500">Netflix Clone</h1>
-			<p className="text-white">Logged in as: {user?.email}</p>
-			<button className="h-10 w-full bg-white" onClick={() => signOut()}>
-				Logout
-			</button>
-		</div>
-	)
+	return <>
+		<Navbar />
+	</>
 }
