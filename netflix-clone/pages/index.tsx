2a68d69579c83d1adb4752ca4dda45519c21f64c
Thibesan Raveenderan <traveen2@uwo.ca>
2023-06-07 18:31:48 -0400

    Movie Card Setup for Favorites

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -1,8 +1,10 @@
-import useCurrentUser from '@/hooks/useCurrentUser'
 import { NextPageContext } from 'next'
 import { getSession, signOut } from 'next-auth/react'
 import Navbar from '../components/Navbar'
 import Billboard from '../components/Billboard'
+import MovieList from '../components/MovieList'
+import useMovieList from '../hooks/useMovieList'
+import useFavorites from '../hooks/useFavorites'
 
 export async function getServerSideProps(context: NextPageContext) {
 	const session = await getSession(context)
@@ -22,10 +24,16 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-	const { data: user } = useCurrentUser()
+	const { data: movies = [] } = useMovieList()
+	const { data: favorites = [] } = useFavorites()
 
 	return <>
 		<Navbar />
 		<Billboard />
+		<div className="pb-40">
+			<MovieList title='Trending Now' data={movies} />
+			<MovieList title='My List' data={favorites}/>
+
+		</div>
 	</>
 }
